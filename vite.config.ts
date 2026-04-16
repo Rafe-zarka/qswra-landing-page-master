30fe5d94d4535fbe94a18f903c4d391872f1b60a
Rafe-zarka <aboalreef1999@gmail.com>
2026-04-16 16:07:17 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/qswra-landing-page-master/',
   plugins: [react()],
   resolve: {
     alias: {
